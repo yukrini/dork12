ebe2b9149bc825b5775e020a9b4fbb8689a0a139
dork12 <127926288+yukrini@users.noreply.github.com>
2024-02-29 22:56:36 +0700

    nganu.js

---
+++ nganu.js
@@ -0,0 +1,11 @@
+		if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/Windows/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//pl22656807.profitablegatecpm.com/9e/8b/c3/9e8bc3ca8a6da4b3e5bdb2972bcd7ccd.js',document.body||document.documentElement)
+	 }
+	 
+	
